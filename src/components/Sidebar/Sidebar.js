390e10be6e356be9fd2b63346fbe9380d011a1f0
WhjteFox <burlak.denys@student.uzhnu.edu.ua>
2024-05-18 19:30:45 +0300

    Sidebar: add icons

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,21 +6,33 @@ export const Sidebar = () => {
         <div className="side-bar">
             <div className="side-navbar">
                 <div className="d-flex align-items-center px-4 py-3 rounded-pill">
+                    <span className="me-3 position-relative">
+                        <img src={require('../image/icons/sidebar_home_filled.png')} alt=""></img>
+                    </span>
                     <span className="fw-bold">
                         <a href="/">Головна</a>
                     </span>
                 </div>
                 <div className="d-flex align-items-center px-4 py-3 rounded-pill">
+                    <span className="me-3 position-relative">
+                        <img src={require('../image/icons/sidebar_likes_outline.png')} alt=""></img>
+                    </span>
                     <span>
                         <a href="/">Вподобання</a>
                     </span>
                 </div>
                 <div className="d-flex align-items-center px-4 py-3 rounded-pill">
+                    <span className="me-3 position-relative">
+                        <img src={require('../image/icons/sidebar_bookings_outline.png')} alt=""></img>
+                    </span>
                     <span>
                         <a href="/">Бронювання</a>
                     </span>
                 </div>
                 <div className="d-flex align-items-center px-4 py-3 rounded-pill">
+                    <span className="me-3 position-relative">
+                        <img src={require('../image/icons/sidebar_profile_outline.png')} alt=""></img>
+                    </span>
                     <span>
                         <a href="/">Профіль</a>
                     </span>
